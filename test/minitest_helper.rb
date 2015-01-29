b3d78a0f79b8c3318ad2a6c8708fd7e61b96a07e
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-01-29 09:37:13 -0500

    updating path to firefox in last commit

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -14,7 +14,7 @@ MED_BASE_URL = Configuration["medtronic"]["base_url"]
 COLLECTION_URL = Configuration["collection_url"]
 
 def firefox
-  Selenium::WebDriver::Firefox::Binary.path= '/opt/firefox/firefox'
+  # Selenium::WebDriver::Firefox::Binary.path= '/opt/firefox/firefox'
   # Selenium::WebDriver::Firefox::Binary.path= '/Applications/Firefox.app/Contents/MacOS/firefox'
   @driver = Selenium::WebDriver.for :firefox
   @driver.manage.window.resize_to(1024,728)
