d34a973e2ac9388c0a7a04c25a473a37d0d55992
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-01-29 10:33:05 -0500

    moving the browser mob proxy on the test server and updating the tests to use the new location

--- test/config/proxy/settings.rb
+++ test/config/proxy/settings.rb
@@ -1,6 +1,6 @@
 class Settings
   def self.location
-    "/home/ubuntu/Downloads/browsermob-proxy-2.0-beta-9/bin/browsermob-proxy"
+    "/usr/local/automated_tests/proxy/browsermob-proxy-2.0-beta-9/bin/browsermob-proxy"
     # "/Users/rhuberdeau/Downloads/browsermob-proxy-2.0-beta-9/bin/browsermob-proxy"
   end
 end
