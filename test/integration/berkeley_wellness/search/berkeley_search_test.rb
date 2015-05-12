6c5fb85acac14b8e16787a9a8bae86768299e994
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-05-12 13:31:04 -0400

    left the search test commented out

--- test/integration/berkeley_wellness/search/berkeley_search_test.rb
+++ test/integration/berkeley_wellness/search/berkeley_search_test.rb
@@ -10,15 +10,15 @@ class BerkeleyTagHomeTest < MiniTest::Test
       @page = ::BerkeleySlideShowPage.new(:driver =>@driver, :proxy => @proxy)
     end
 
-    # should "produce results" do
-    #   element = @driver.find_element(:css, "#show_searchform")
-    #   element.click
-    #   search_form = @driver.find_element(:css, "#edit-search-block-form--2")
-    #   search_form.send_keys "exercise"
-    #   search_form.submit
-    #   wait_for_page_to_load
-    #   assert_equal(true, (@driver.find_elements(:css, "#block-system-main > div > section > div.article_listing_list > ul > li > article > div > h2 > a").length) >= 1)
-    # end
+    should "produce results" do
+      element = @driver.find_element(:css, "#show_searchform")
+      element.click
+      search_form = @driver.find_element(:css, "#edit-search-block-form--2")
+      search_form.send_keys "exercise"
+      search_form.submit
+      wait_for_page_to_load
+      assert_equal(true, (@driver.find_elements(:css, "#block-system-main > div > section > div.article_listing_list > ul > li > article > div > h2 > a").length) >= 1)
+    end
 
     ##################################################################
     ################### ASSETS #######################################
