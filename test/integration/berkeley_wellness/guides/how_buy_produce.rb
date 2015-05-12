a5752155fe12600124c24867a65b78d0d08750a7
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-05-12 11:52:08 -0400

    Adding Guides with an assets test. Adding teardown to hard_look_bpa

--- test/integration/berkeley_wellness/guides/how_buy_produce.rb
+++ test/integration/berkeley_wellness/guides/how_buy_produce.rb
@@ -1,12 +1,12 @@
 require_relative '../../../minitest_helper' 
 require_relative '../../../pages/berkeley/berkeley_slide_show_page'
 
-class BerkeleyExplainerTest < MiniTest::Test
-  context "hard look bpa" do 
+class BerkeleyGuidesTest < MiniTest::Test
+  context "how to buy produce" do 
     setup do
       fire_fox_with_secure_proxy
       @proxy.new_har
-      visit "#{BW_BASE_URL}/healthy-community/environmental-health/article/hard-look-bpa"
+      visit "#{BW_BASE_URL}/healthy-eating/food/article/how-buy-produce"
       @page = ::BerkeleySlideShowPage.new(:driver =>@driver, :proxy => @proxy)
     end
 
