552edcf3da1112e8e2dc654c5f9ca784286e252a
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-05-12 13:21:42 -0400

    adding Guids Home and Category Home

---
+++ test/integration/berkeley_wellness/guides_home/supermaket_buying_guide.rb
@@ -0,0 +1,28 @@
+require_relative '../../../minitest_helper' 
+require_relative '../../../pages/berkeley/berkeley_slide_show_page'
+
+class BerkeleyGuidesHomeTest < MiniTest::Test
+  context "supermarket buying guide" do 
+    setup do
+      fire_fox_with_secure_proxy
+      @proxy.new_har
+      visit "#{BW_BASE_URL}/healthy-eating/supermarket-buying-guide"
+      @page = ::BerkeleySlideShowPage.new(:driver =>@driver, :proxy => @proxy)
+    end
+
+    ##################################################################
+    ################### ASSETS #######################################
+    context "assets" do 
+      should "have valid assets" do 
+        assets = @page.assets
+        assets.validate
+        assert_equal(true, assets.errors.empty?, "#{assets.errors.messages}")
+      end
+    end
+  end
+
+  def teardown  
+    @driver.quit  
+    @proxy.close
+  end 
+end
