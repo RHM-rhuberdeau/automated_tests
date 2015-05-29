2c282494695b9b300ffc59f9e621663e2206fa51
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-05-29 12:21:12 -0400

    Adding a sponsored topic mobile slideshow test and fixing the routing_error test

--- test/pages/healthcentral/concerns/ads_test_cases.rb
+++ test/pages/healthcentral/concerns/ads_test_cases.rb
@@ -5,6 +5,11 @@ module HealthCentralAds
     validate :unique_ads_per_page_view
     validate :correct_ad_site
     validate :correct_ad_categories
+    validate :exclusion_cat
+    validate :sponsor_kw
+    validate :thcn_content_type
+    validate :thcn_super_cat
+    validate :thcn_category
     validate :pharma_safe
     validate :ugc
 
@@ -13,10 +18,12 @@ module HealthCentralAds
       @proxy                  = args[:proxy]
       @url                    = args[:url]
       @ad_site                = args[:ad_site]
-      @expected_ad_site       = args[:expected_ad_site]
       @ad_categories          = args[:ad_categories]
-      @expected_ad_categories = args[:expected_ad_categories]
-      @pharma_safe            = args[:pharma_safe]
+      @exclusion_cat          = args[:exclusion_cat]
+      @sponsor_kw             = args[:sponsor_kw]
+      @thcn_content_type      = args[:thcn_content_type]
+      @thcn_super_cat         = args[:thcn_super_cat]
+      @thcn_category          = args[:thcn_category]
       @ugc                    = args[:ugc]
     end
 
@@ -37,35 +44,67 @@ module HealthCentralAds
       ord_values_2 = ads_from_page2.collect(&:ord).uniq
 
       unless ord_values_1.length == 1
-        self.errors.add(:base, "Ads on the first view had multiple ord values: #{ord_values_1}")
+        self.errors.add(:ads, "Ads on the first view had multiple ord values: #{ord_values_1}")
       end
       unless ord_values_2.length == 1
-        self.errors.add(:base, "Ads on the second view had multiple ord values: #{ord_values_2}")
+        self.errors.add(:ads, "Ads on the second view had multiple ord values: #{ord_values_2}")
       end
       unless (ord_values_1[0] != ord_values_2[0])
-        self.errors.add(:base, "Ord values did not change on page reload: #{ord_values_1} #{ord_values_2}")
+        self.errors.add(:ads, "Ord values did not change on page reload: #{ord_values_1} #{ord_values_2}")
       end
     end
 
     def correct_ad_site
-      unless @ad_site == @expected_ad_site
-        self.errors.add(:base, "ad_site was #{@ad_site} not #{@expected_ad_site}")
+      ad_site = evaluate_script "AD_SITE"
+      unless ad_site == @ad_site
+        self.errors.add(:ads, "ad_site was #{ad_site} not #{@ad_site}")
       end
     end
 
     def correct_ad_categories
-      unless @ad_categories == @expected_ad_categories
-        self.errors.add(:base, "ad_categories was #{@ad_categories} not #{@expected_ad_categories}")
+      ad_categories = evaluate_script "AD_CATEGORIES"
+      unless ad_categories == @ad_categories
+        self.errors.add(:ads, "ad_categories was #{ad_categories} not #{@ad_categories}")
+      end
+    end
+
+    def exclusion_cat
+      exclusion_cat = evaluate_script "EXCLUSION_CAT"
+      unless exclusion_cat == @exclusion_cat
+        self.errors.add(:ads, "EXCLUSION_CAT was #{exclusion_cat} not #{@exclusion_cat}")
+      end
+    end
+
+    def sponsor_kw
+      sponsor_kw = evaluate_script "SPONSOR_KW"
+      unless sponsor_kw == @sponsor_kw
+        self.errors.add(:ads, "SPONSOR_KW was #{sponsor_kw} not #{@sponsor_kw}")
+      end
+    end
+
+    def thcn_content_type
+      thcn_content_type = evaluate_script "THCN_CONTENT_TYPE"
+      unless thcn_content_type == @thcn_content_type
+        self.errors.add(:ads, "THCN_CONTENT_TYPE was #{thcn_content_type} not #{@thcn_content_type}")
+      end
+    end
+
+    def thcn_super_cat
+      thcn_super_cat = evaluate_script "THCN_SUPER_CAT"
+      unless thcn_super_cat == @thcn_super_cat
+        self.errors.add(:ads, "THCN_SUPER_CAT was #{thcn_super_cat} not #{@thcn_super_cat}")
+      end
+    end
+
+    def thcn_category
+      thcn_category = evaluate_script "THCN_CATEGORY"
+      unless thcn_category == @thcn_category
+        self.errors.add(:ads, "THCN_CATEGORY was #{thcn_category} not #{@thcn_category}")
       end
     end
 
     def pharma_safe
-      if @pharma_safe
-        pharma_safe = @driver.execute_script("return EXCLUSION_CAT") != 'community'
-        unless pharma_safe == @pharma_safe
-          self.errors.add(:base, "Expected pharma_safe to be #{@pharma_safe}")
-        end
-      end
+
     end
 
     def ugc
@@ -81,10 +120,10 @@ module HealthCentralAds
 
       ugc_values = ugc_values.uniq.to_s
       unless ugc_values == @ugc
-        self.errors.add(:base, "ugc was #{ugc_values} not #{@ugc}")
+        self.errors.add(:ads, "ugc was #{ugc_values} not #{@ugc}")
       end
       unless has_file == true
-        self.errors.add(:base, "namespace.js was not loaded")
+        self.errors.add(:ads, "namespace.js was not loaded")
       end
     end
   end
--- test/pages/healthcentral/concerns/header.rb
+++ test/pages/healthcentral/concerns/header.rb
@@ -432,4 +432,45 @@ module HealthCentralHeader
       end
     end
   end
+
+  class SPMobile < MobileHeader
+    include ::ActiveModel::Validations
+
+    # validate :logo
+    validate :title_link
+    # validate :more_on_link
+
+    def initialize(args)
+      @driver       =args[:driver]
+      @logo         = args[:logo]
+      @title_link   = args[:title_link]
+      @more_on_link = args[:more_on_link]
+      @subcategory  = args[:sub_category]
+      @related      = args[:related]
+    end
+
+    def logo
+      logo = find ".Logo-supercollection img"
+      logo_img = logo.attribute('src') if logo
+      unless logo_img == @logo
+        self.errors.add(:base, "Logo image src was #{logo_img} not #{@logo}")
+      end
+    end
+
+    def title_link
+      title_link = find "a.title-supercollection"
+      title_text = title_link.text if title_link
+      unless title_text == @title_link
+        self.errors.add(:base, "Title link was #{title_text} not #{@title_link}")
+      end
+    end
+
+    def more_on_link
+      more_on_link = find "span.more-supercollection"
+      link         = more_on_link.text if more_on_link
+      unless link == @more_on_link
+        self.errors.add(:base, "More on link was #{link} not #{@more_on_link}")
+      end
+    end
+  end
 end
