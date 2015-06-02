7dbc47559195109101d868b5ab0f5d164ff1bb1b
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-06-01 21:03:28 -0400

    Adding phase and topic tests

---
+++ test/integration/healthcentral/slideshows/living_well_with_migraines_mobile.rb
@@ -0,0 +1,70 @@
+require_relative '../../../minitest_helper' 
+require_relative '../../../pages/healthcentral/mobile_slideshow'
+
+class SlideshowTest < MiniTest::Test
+  context "a mobile sponsored topic slideshow, living well with migraines" do 
+    setup do 
+      mobile_fire_fox_with_secure_proxy
+      @proxy.new_har
+      io = File.open('test/fixtures/healthcentral/slideshows.yml')
+      slideshow_fixture = YAML::load_documents(io)
+      @fixture = OpenStruct.new(slideshow_fixture[0]['living_well_mobile'])
+      head_navigation = HealthCentralHeader::SPMobile.new(:logo => "#{ASSET_HOST}com/sites/all/themes/healthcentral/images/logo_lbln.png", 
+                                   :title_link => "Taking Control of Chronic Migraine",
+                                   :driver => @driver)
+      footer          = HealthCentralFooter::RedesignFooter.new(:driver => @driver)
+      @page = HealthCentralMobileSlideshow::MobileSlideshowPage.new(:driver =>@driver,:proxy => @proxy, :fixture => @fixture, :head_navigation => head_navigation, :footer => footer, :collection => true)
+      visit "#{HC_BASE_URL}/migraine/cf/slideshows/12-tips-for-living-well-with-migraines?ic=recc"
+    end
+
+    ##################################################################
+    ################ FUNCTIONALITY ###################################
+    context "when functioning properly" do 
+      should "not have any errors" do 
+        functionality = @page.functionality
+        functionality.validate
+        assert_equal(true, functionality.errors.empty?, "#{functionality.errors.messages}")
+      end
+    end
+
+    ##################################################################
+    ################### ASSETS #######################################
+    context "assets" do 
+      should "have valid assets" do 
+        assets = @page.assets
+        assets.validate
+        assert_equal(true, assets.errors.empty?, "#{assets.errors.messages}")
+      end
+    end
+
+    #########################################################################
+    ################### ADS, ANALYTICS, OMNITURE ############################
+    context "omniture" do
+      should "not have any errors" do 
+        ad_site        = "cm.own.tcc"
+        ad_categories  = ["zecuity", "", ""]
+        ads_test_cases = @page.ads_test_cases(:ad_site => ad_site, :ad_categories => ad_categories)
+        omniture       = @page.omniture
+
+        ads_test_cases.validate
+        omniture.validate
+        assert_equal(true, (ads_test_cases.errors.empty? && omniture.errors.empty?), "#{ads_test_cases.errors.messages} #{omniture.errors.messages}")
+      end
+    end
+
+    ##################################################################
+    ################### GLOBAL SITE TESTS ############################
+    context "Global Site tests" do 
+      should "have passing global test cases" do 
+        global_test_cases = @page.global_test_cases
+        global_test_cases.validate
+        assert_equal(true, global_test_cases.errors.empty?, "#{global_test_cases.errors.messages}")
+      end
+    end
+  end
+
+  def teardown  
+    @driver.quit  
+    @proxy.close
+  end 
+end
---
+++ test/integration/healthcentral/topics/ibd_introduction_symptoms.rb
@@ -0,0 +1,94 @@
+require_relative '../../../minitest_helper' 
+require_relative '../../../pages/healthcentral/topic_page'
+
+class DecreasedSmellAndTastePageTest < MiniTest::Test
+  context "ibd introduction" do 
+    setup do 
+      fire_fox_with_secure_proxy
+      @proxy.new_har
+      io = File.open('test/fixtures/healthcentral/topics.yml')
+      fixture           = YAML::load_documents(io)
+      topic_fixture     = OpenStruct.new(fixture[0]['ibd_symptoms'])
+      head_navigation   = HealthCentralHeader::RedesignHeader.new(:logo => "#{ASSET_HOST}/sites/all/themes/healthcentral/images/logo_lbln.png", 
+                                   :sub_category => "Digestive Health",
+                                   :related => ['Acid Refulx'],
+                                   :driver => @driver)
+      footer            = HealthCentralFooter::RedesignFooter.new(:driver => @driver)
+      @page             = ::Topics::TopicPage.new(:driver => @driver,:proxy => @proxy,:fixture => topic_fixture, :head_navigation => head_navigation, :footer => footer, :collection => false)
+      visit "#{HC_BASE_URL}/ibd/d/introduction/symptoms"
+    end
+
+    ##################################################################
+    ################ FUNCTIONALITY ###################################
+    context "when functioning properly" do 
+      should "not have any errors" do 
+        functionality = @page.functionality(:driver => @driver, :phase => "introduction", :phase_navigation => ['Introduction', 'Diagnosis', '', 'Living With', 'Treatment', 'Care', 'Related Conditions'])
+        functionality.validate
+        assert_equal(true, functionality.errors.empty?, "#{functionality.errors.messages}")
+      end
+    end
+
+    ##################################################################
+    ################### ASSETS #######################################
+    context "assets" do 
+      should "have valid assets" do 
+        assets = @page.assets
+        assets.validate
+        assert_equal(true, assets.errors.empty?, "#{assets.errors.messages}")
+      end
+    end
+
+    ##################################################################
+    ################### SEO ##########################################
+    context "SEO" do 
+      should "have the correct title" do 
+        assert_equal("Inflammatory Bowel Disease | www.healthcentral.com", @driver.title)
+      end
+    end
+
+    #########################################################################
+    ################### ADS, ANALYTICS, OMNITURE ############################
+    context "ads, analytics, omniture" do
+      should "not have any errors" do 
+        ad_site           = 'cm.ver.ibd'
+        ad_categories     = ["", "", '']
+        exclusion_cat     = ""
+        sponsor_kw        = ''
+        thcn_content_type = ""
+        thcn_super_cat    = "Body & Mind"
+        thcn_category     = "Digestive Health"
+        ads                     = Topics::TopicPage::AdsTestCases.new(:driver => @driver,
+                                                                     :proxy => @proxy, 
+                                                                     :url => "#{HC_BASE_URL}/ibd/d/introduction/symptoms",
+                                                                     :ad_site => ad_site,
+                                                                     :ad_categories => ad_categories,
+                                                                     :exclusion_cat => exclusion_cat,
+                                                                     :sponsor_kw  => sponsor_kw,
+                                                                     :thcn_content_type => thcn_content_type,
+                                                                     :thcn_super_cat => thcn_super_cat,
+                                                                     :thcn_category => thcn_category,
+                                                                     :ugc => "[\"n\"]") 
+        ads.validate
+
+        omniture = @page.omniture
+        omniture.validate
+        assert_equal(true, (ads.errors.empty? && omniture.errors.empty?), "#{ads.errors.messages} #{omniture.errors.messages}")
+      end
+    end
+
+    ##################################################################
+    ################### GLOBAL SITE TESTS ############################
+    context "Global Site tests" do 
+      should "have passing global test cases" do 
+        global_test_cases = @page.global_test_cases
+        global_test_cases.validate
+        assert_equal(true, global_test_cases.errors.empty?, "#{global_test_cases.errors.messages}")
+      end
+    end
+  end
+
+  def teardown  
+    @driver.quit  
+    @proxy.close
+  end 
+end
