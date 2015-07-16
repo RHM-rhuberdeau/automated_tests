027e8d0dcd20053cd06a8ccfd324dcde2b418467
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-07-16 13:46:43 -0400

    Adding current omniture values from production. Adding the proxy to the tests, using the correct browser proxy combination now

--- test/integration/healthcentral/fdb/acid_reducer.rb
+++ test/integration/healthcentral/fdb/acid_reducer.rb
@@ -4,10 +4,10 @@ require_relative '../../../pages/healthcentral/fdb_page'
 class FdbMedicationPageTest < MiniTest::Test
   context "acid reducer" do 
     setup do 
-      mobile_fire_fox_with_secure_proxy
-      io = File.open('test/fixtures/healthcentral/fdb.yml')
-      fixture           = YAML::load_documents(io)
-      topic_fixture     = OpenStruct.new(fixture[0]['acid_reducer'])
+      fire_fox_with_secure_proxy
+      io          = File.open('test/fixtures/healthcentral/fdb.yml')
+      fixture     = YAML::load_documents(io)
+      fdb_fixture = OpenStruct.new(fixture[0]['acid_reducer'])
     end
   end
 
--- test/integration/healthcentral/fdb/acid_reflux_medications.rb
+++ test/integration/healthcentral/fdb/acid_reflux_medications.rb
@@ -4,10 +4,11 @@ require_relative '../../../pages/healthcentral/fdb_page'
 class FdbMedicationsIndexPageTest < MiniTest::Test
   context "acid reflux" do 
     setup do 
-      mobile_fire_fox_with_secure_proxy
-      io = File.open('test/fixtures/healthcentral/fdb.yml')
-      fixture           = YAML::load_documents(io)
-      topic_fixture     = OpenStruct.new(fixture[0]['acid_reflux'])
+      fire_fox_with_secure_proxy
+      @proxy.new_har
+      io          = File.open('test/fixtures/healthcentral/fdb.yml')
+      fixture     = YAML::load_documents(io)
+      fdb_fixture = OpenStruct.new(fixture[0]['acid_reflux'])
     end
   end
 
