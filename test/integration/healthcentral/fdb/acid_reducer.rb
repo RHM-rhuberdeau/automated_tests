c6473665e245d7d26e92245dc9164261a67a3dd2
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-07-16 13:51:19 -0400

    Adding a header and footer to the page object. Adding the correct url for each test. Adding an omniture test for each FDB page

--- test/integration/healthcentral/fdb/acid_reducer.rb
+++ test/integration/healthcentral/fdb/acid_reducer.rb
@@ -8,6 +8,23 @@ class FdbMedicationPageTest < MiniTest::Test
       io          = File.open('test/fixtures/healthcentral/fdb.yml')
       fixture     = YAML::load_documents(io)
       fdb_fixture = OpenStruct.new(fixture[0]['acid_reducer'])
+      head_navigation   = HealthCentralHeader::RedesignHeader.new(:logo => "#{ASSET_HOST}/sites/all/themes/healthcentral/images/logo_lbln.png", 
+                                   :sub_category => "Acid Reflux",
+                                   :related => [''],
+                                   :driver => @driver)
+      footer            = HealthCentralFooter::RedesignFooter.new(:driver => @driver)
+      @page             = FDB::FDBPage.new(:driver => @driver,:proxy => @proxy,:fixture => fdb_fixture, :head_navigation => head_navigation, :footer => footer, :collection => false)
+      visit "#{HC_BASE_URL}/acid-reflux/medications/acid-reducer-famotidine-oral-153189"
+    end
+
+    #########################################################################
+    ################### ADS, ANALYTICS, OMNITURE ############################
+    context "ads, analytics, omniture" do
+      should "not have any errors" do
+        omniture = @page.omniture
+        omniture.validate
+        assert_equal(true, omniture.errors.empty?, "#{omniture.errors.messages}")
+      end
     end
   end
 
--- test/integration/healthcentral/fdb/acid_reflux_medications.rb
+++ test/integration/healthcentral/fdb/acid_reflux_medications.rb
@@ -6,9 +6,26 @@ class FdbMedicationsIndexPageTest < MiniTest::Test
     setup do 
       fire_fox_with_secure_proxy
       @proxy.new_har
-      io          = File.open('test/fixtures/healthcentral/fdb.yml')
-      fixture     = YAML::load_documents(io)
-      fdb_fixture = OpenStruct.new(fixture[0]['acid_reflux'])
+      io                = File.open('test/fixtures/healthcentral/fdb.yml')
+      fixture           = YAML::load_documents(io)
+      fdb_fixture       = OpenStruct.new(fixture[0]['acid_reflux'])
+      head_navigation   = HealthCentralHeader::RedesignHeader.new(:logo => "#{ASSET_HOST}/sites/all/themes/healthcentral/images/logo_lbln.png", 
+                                   :sub_category => "Acid Reflux",
+                                   :related => [''],
+                                   :driver => @driver)
+      footer            = HealthCentralFooter::RedesignFooter.new(:driver => @driver)
+      @page             = FDB::FDBPage.new(:driver => @driver,:proxy => @proxy,:fixture => fdb_fixture, :head_navigation => head_navigation, :footer => footer, :collection => false)
+      visit "#{HC_BASE_URL}/acid-reflux/medications"
+    end
+
+    #########################################################################
+    ################### ADS, ANALYTICS, OMNITURE ############################
+    context "ads, analytics, omniture" do
+      should "not have any errors" do
+        omniture = @page.omniture
+        omniture.validate
+        assert_equal(true, omniture.errors.empty?, "#{omniture.errors.messages}")
+      end
     end
   end
 
