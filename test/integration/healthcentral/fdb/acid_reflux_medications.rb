6fe97c026f0d2519b7c8719131ccebe0dcb1671a
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-07-16 13:29:25 -0400

    Connection both FDB tests to the correct fixture

--- test/integration/healthcentral/fdb/acid_reflux_medications.rb
+++ test/integration/healthcentral/fdb/acid_reflux_medications.rb
@@ -5,6 +5,9 @@ class FdbMedicationsIndexPageTest < MiniTest::Test
   context "acid reflux" do 
     setup do 
       mobile_fire_fox_with_secure_proxy
+      io = File.open('test/fixtures/healthcentral/fdb.yml')
+      fixture           = YAML::load_documents(io)
+      topic_fixture     = OpenStruct.new(fixture[0]['acid_reflux'])
     end
   end
 
