e5771a41a68b33018dfc8d75a5e89c8eab5418cf
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-07-16 23:56:55 -0400

    Adding tests for lazy loading ads

--- test/pages/healthcentral/concerns/ads_test_cases.rb
+++ test/pages/healthcentral/concerns/ads_test_cases.rb
@@ -127,4 +127,41 @@ module HealthCentralAds
       end
     end
   end
+
+  class LazyLoadedAds < AdsTestCases
+    include ::ActiveModel::Validations
+
+    def unique_ads_per_page_view
+      scroll1 = 4000
+      scroll2 = 4000
+      @ads = {}
+      @driver.execute_script "window.scrollBy(0, #{scroll1})"
+      sleep 1
+      @all_ads = HealthCentralPage.get_all_ads(@proxy)
+      @ads[1] = @all_ads
+      @driver.execute_script "window.scrollBy(0, #{scroll2})"
+      sleep 1
+      @all_ads = HealthCentralPage.get_all_ads(@proxy)
+      @ads[2] = @all_ads - @ads.flatten(2)
+
+      ads_from_page1 = @ads[1].map { |ad| HealthCentralAds::Ads.new(ad) }
+      ads_from_page2 = @ads[2].map { |ad| HealthCentralAds::Ads.new(ad) }
+
+      ord_values_1 = ads_from_page1.collect(&:ord).uniq
+      ord_values_2 = ads_from_page2.collect(&:ord).uniq
+
+      unless ads_from_page1.length == 1
+        self.errors.add(:ads, "Wrong number of ads were loaded on first scroll. Expeced 1 got #{ads_from_page1.length}")
+      end
+      unless ord_values_1.length == 1
+        self.errors.add(:ads, "First set of lazy loaded ads had multiple ord values: #{ord_values_1}")
+      end
+      unless ord_values_2.length == 1
+        self.errors.add(:ads, "Ads on the second view had multiple ord values: #{ord_values_2}")
+      end
+      unless (ord_values_1[0] != ord_values_2[0])
+        self.errors.add(:ads, "Ord values did not change on page reload: #{ord_values_1} #{ord_values_2}")
+      end
+    end
+  end
 end
--- test/pages/healthcentral/fdb_page.rb
+++ test/pages/healthcentral/fdb_page.rb
@@ -2,8 +2,12 @@ require_relative './healthcentral_page'
 
 module FDB
   class FDBPage < HealthCentralPage
-    
+    def initialize(args)
+      @driver           = args[:driver]
+      @proxy            = args[:proxy]
+      @fixture          = args[:fixture]
+      @head_navigation  = args[:head_navigation]
+      @footer           = args[:footer]
+    end
   end
-
-  
 end
