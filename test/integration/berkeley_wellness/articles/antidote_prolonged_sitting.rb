3a61e28d9b9df175f67c5b89a04e3f29801aea70
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-07-31 11:28:16 -0400

    Adding assets, ads and omniture tests to Berkeley articles

--- test/integration/berkeley_wellness/articles/antidote_prolonged_sitting.rb
+++ test/integration/berkeley_wellness/articles/antidote_prolonged_sitting.rb
@@ -1,13 +1,17 @@
 require_relative '../../../minitest_helper' 
-require_relative '../../../pages/berkeley/berkeley_slide_show_page'
+require_relative '../../../pages/berkeley/article_page'
 
 class BerkeleyArticleTest < MiniTest::Test
   context "antidote prolonged sitting" do 
     setup do
       fire_fox_with_secure_proxy
       @proxy.new_har
-      visit "#{BW_BASE_URL}/fitness/active-lifestyle/article/antidote-prolonged-sitting"
-      @page = BerkeleySlideShowPage.new(:driver => @driver, :proxy => @proxy)
+      io                = File.open('test/fixtures/berkeley/articles.yml')
+      fixture           = YAML::load_documents(io)
+      article_fixture   = OpenStruct.new(fixture[0]['prolonged_sitting'])
+      @page             = Articles::ArticlePage.new(:driver => @driver,:proxy => @proxy,:fixture => article_fixture)
+      @url              = "#{BW_BASE_URL}/fitness/active-lifestyle/article/antidote-prolonged-sitting"
+      visit @url
     end
 
     ##################################################################
@@ -27,6 +31,37 @@ class BerkeleyArticleTest < MiniTest::Test
         seo = @page.seo 
         seo.validate
         assert_equal(true, seo.errors.empty?, "#{seo.errors.messages}")
+      end
+    end
+
+    #########################################################################
+    ################### ADS, ANALYTICS, OMNITURE ############################
+    context "ads, analytics, omniture" do
+      should "not have any errors" do 
+        ad_site           = 'cm.pub.berkwell'
+        ad_categories     = ["fitness","activelifestyle"]
+        exclusion_cat     = ""
+        sponsor_kw        = ''
+        thcn_content_type = "article"
+        thcn_super_cat    = ""
+        thcn_category     = ""
+        ads               = Articles::ArticlePage::AdsTestCases.new(:driver => @driver,
+                                                                 :proxy => @proxy, 
+                                                                 :url => @url,
+                                                                 :ad_site => ad_site,
+                                                                 :ad_categories => ad_categories,
+                                                                 :exclusion_cat => exclusion_cat,
+                                                                 :sponsor_kw  => sponsor_kw,
+                                                                 :thcn_content_type => thcn_content_type,
+                                                                 :thcn_super_cat => thcn_super_cat,
+                                                                 :thcn_category => thcn_category,
+                                                                 :ugc => "[\"n\"]") 
+        ads.validate
+
+        omniture = @page.omniture
+
+        omniture.validate
+        assert_equal(true, (ads.errors.empty? && omniture.errors.empty?), "#{ads.errors.messages} #{omniture.errors.messages}")
       end
     end
   end
--- test/integration/berkeley_wellness/articles/future_personalized_medicine.rb
+++ test/integration/berkeley_wellness/articles/future_personalized_medicine.rb
@@ -1,13 +1,17 @@
 require_relative '../../../minitest_helper' 
-require_relative '../../../pages/berkeley/berkeley_slide_show_page'
+require_relative '../../../pages/berkeley/article_page'
 
 class BerkeleyBlogTest < MiniTest::Test
   context "future personalized medicine" do 
     setup do
       fire_fox_with_secure_proxy
       @proxy.new_har
-      visit "#{BW_BASE_URL}/healthy-community/health-care-policy/article/future-personalized-medicine"
-      @page = ::BerkeleySlideShowPage.new(:driver =>@driver, :proxy => @proxy)
+      io                = File.open('test/fixtures/berkeley/articles.yml')
+      fixture           = YAML::load_documents(io)
+      article_fixture   = OpenStruct.new(fixture[0]['future_medicine'])
+      @page             = Articles::ArticlePage.new(:driver => @driver,:proxy => @proxy,:fixture => article_fixture)
+      @url              = "#{BW_BASE_URL}/healthy-community/health-care-policy/article/future-personalized-medicine"
+      visit @url
     end
 
     ##################################################################
@@ -17,6 +21,47 @@ class BerkeleyBlogTest < MiniTest::Test
         assets = @page.assets
         assets.validate
         assert_equal(true, assets.errors.empty?, "#{assets.errors.messages}")
+      end
+    end
+
+    ##################################################################
+    ################### SEO ##########################################
+    context "SEO" do 
+      should "have the correct title" do 
+        seo = @page.seo 
+        seo.validate
+        assert_equal(true, seo.errors.empty?, "#{seo.errors.messages}")
+      end
+    end
+
+    #########################################################################
+    ################### ADS, ANALYTICS, OMNITURE ############################
+    context "ads, analytics, omniture" do
+      should "not have any errors" do 
+        ad_site           = 'cm.pub.berkwell'
+        ad_categories     = ["selfcare","preventivecare","healthcarepolicy"]
+        exclusion_cat     = ""
+        sponsor_kw        = ''
+        thcn_content_type = "blog"
+        thcn_super_cat    = ""
+        thcn_category     = ""
+        ads               = Articles::ArticlePage::AdsTestCases.new(:driver => @driver,
+                                                                 :proxy => @proxy, 
+                                                                 :url => @url,
+                                                                 :ad_site => ad_site,
+                                                                 :ad_categories => ad_categories,
+                                                                 :exclusion_cat => exclusion_cat,
+                                                                 :sponsor_kw  => sponsor_kw,
+                                                                 :thcn_content_type => thcn_content_type,
+                                                                 :thcn_super_cat => thcn_super_cat,
+                                                                 :thcn_category => thcn_category,
+                                                                 :ugc => "[\"n\"]") 
+        ads.validate
+
+        omniture = @page.omniture
+
+        omniture.validate
+        assert_equal(true, (ads.errors.empty? && omniture.errors.empty?), "#{ads.errors.messages} #{omniture.errors.messages}")
       end
     end
   end
