e8ac93f69e49892d1ae4e4d39cc13b259ed34898
RHM-rchu <ron@remedyhealthmedia.com>
2015-08-05 08:47:13 -0400

    fix potentially memory leak and run away URL fetches

--- scripts/status.code.check.py
+++ scripts/status.code.check.py
@@ -13,19 +13,18 @@ import sys, getopt
 
 #arg_other_test_subdomain = input("Enter a subdomain to test agains: ")
 arg_other_test_subdomain="qa1"
-arg_url_file = '/Users/ron/Dropbox/RHM/tmp/100k.crawl.txt'
+arg_url_file = './100k.crawl.txt'
 arg_stop_after=0
 arg_display_all=0
 arg_use_proxy=0
 arg_just_primary_domain=1
-arg_print_header=1
 arg_concurrent=5
 
 def args_help():
     print "\n"
     print "Usage:"
     print "\t-f list.txt : URL file location "
-    print "\t-0 0 : Set to '0' to scan second domain, works with '-s' flag"
+    print "\t-0 1 : Set to '0' to scan second domain, works with '-s' flag"
     print "\t-s qa1 : subdomain URL file prefix, defaul 'qa1', requires '-o 0'"
     print "\t-l 5 : Limit to 'n' in the scan, defaul all"
     print "\t-p 1 : Use proxy server: 10.0.0.10:3128 also set '-s www'"
@@ -36,35 +35,31 @@ def args_help():
     print " '-o 0' scan a second host, default second host is 'qa1'"
     print " '-s qa1'change subdomain of the second to E.G. qa1.healthcentral.com"
     print " '-p 1' using a proxy"
-    print "status.code.check.py -f list_path.txt -l 15 -o0 -s qa1 -p 1"
+    print "status.code.check.py -f list_path.txt -l 15 -o 0 -s qa1 -p 1"
     print "\n"
     sys.exit(0)
 
-if len(sys.argv) < 1:
-    if sys.argv[1] == '-h':
-        args_help()
+
+if sys.argv[1] == '-h':
+    args_help()
 #myopts, args = getopt.getopt(sys.argv[1:],"f:l:s:d:h:p:", ["file", "stop=", "subdomain=", "display", "help", "proxy"])
-myopts, args = getopt.getopt(sys.argv[1:],"f:l:s:d:h:p:o:w:c:")
+myopts, args = getopt.getopt(sys.argv[1:],"f:l:s:d:h:p:o:c:")
 for o, a in myopts:
     if o == '-f':
         arg_url_file=a
     elif o == '-s':
-        arg_other_test_subdomain=a
+        arg_other_test_subdomain=int(a)
     elif o == '-l':
-        arg_stop_after=a
+        arg_stop_after=int(a)
     elif o == '-d':
-        arg_display_all=a
+        arg_display_all=int(a)
     elif o == '-p':
-        arg_use_proxy=a
+        arg_use_proxy=int(a)
         arg_other_test_subdomain='www'
     elif o == '-o':
-        arg_just_primary_domain=a
-    elif o == '-w':
-        arg_print_header=a
+        arg_just_primary_domain=int(a)
     elif o == '-c':
-        arg_concurrent=a
-    elif o == '-h':
-        args_help()
+        arg_concurrent=int(a)
     else:
         print("Don't know what do do with: %s " % o, a)
         args_help()
@@ -73,9 +68,6 @@ for o, a in myopts:
 
 finished=itertools.count(1)
 reactor.suggestThreadPoolSize(arg_concurrent)
-
-if arg_print_header == 1:
-    print 'Status Code,Status Code2,URL,Redirect,Redirect2'
 
 def strip_domain(url):
     # return string.replace(url, 'http://www.healthcentral.com', '')
@@ -87,8 +79,8 @@ def getStatus(ourl):
     conn = httplib.HTTPConnection(url.netloc)
     conn.request("HEAD", url.path)
     res = conn.getresponse()
-    #--- scan second domain
-    if arg_just_primary_domain == '0':
+    if arg_just_primary_domain == 0:
+        #--- scan aother domain
         if arg_use_proxy == 0 :
             other_url = string.replace(ourl, 'www', arg_other_test_subdomain)
             url = urlparse(other_url)
@@ -111,11 +103,12 @@ def processResponse(resp,url):
         location = resp.msg.dict['location']
     if 'server' in resp.msg.dict:
         server = resp.msg.dict['server']
-    if arg_just_primary_domain == '0':
+    if arg_just_primary_domain == 0:
         if 'server' in resp.other.msg.dict:
             server_other = resp.other.msg.dict['server']
         if 'location' in resp.other.msg.dict:
             location_other = resp.other.msg.dict['location']
+
         #--- only care about the diffances
         if (resp.status != resp.other.status) or (arg_display_all <> 0):
             # main:status,other:status,test URI,main:response uri,other:response url
@@ -125,7 +118,6 @@ def processResponse(resp,url):
     processedOne()
 
 def processError(error,url):
-    #print (',,r%,ERROR,' % (url, error) )#, error
     print ',,{0},ERROR: {1},,'.format(url, error)
     processedOne()
 
@@ -138,28 +130,25 @@ def addTask(url):
     req.addCallback(processResponse, url)
     req.addErrback(processError, url)
 
+
+print 'Status Code,Status Code2,URL,Redirect,Redirect2'
+
 added=0
-url_strip=''
 list_urls={}
-#--- loop through urls to scan
-# - remove dupe urls
-# - ensure urls are full path
 for url in open(arg_url_file):
-    url_strip=url.strip()
-    m = re.match('^http://.*', url_strip)
-    if(m == ''):
-        continue
-    if( list_urls.has_key(url_strip) ):
+    if( list_urls.has_key(url.strip()) ):
         continue
     added+=1
-    addTask(url_strip)
-    list_urls[url_strip]=1
-    if "%s" % arg_stop_after == "%s" % added:
+    addTask(url.strip())
+    list_urls[url.strip()]=added
+    #if "%s" % arg_stop_after == "%s" % added:
+    if arg_stop_after >= added:
         break
 try:
     reactor.run()
 except KeyboardInterrupt:
+    print "Interrupted by keyboard. Exiting."
+    #signal.signal(signal.SIGINT, signal.default_int_handler)
     reactor.stop()
-    sys.exit(0)
-
+    os._exit
 
