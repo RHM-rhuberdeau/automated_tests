d7caf27a84a80b56c82cd2a568541ffd5771411c
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-09-18 12:14:36 -0400

    Making sure visit is the last setup action

--- test/integration/berkeley_wellness/tag_home/immune_system.rb
+++ test/integration/berkeley_wellness/tag_home/immune_system.rb
@@ -6,8 +6,9 @@ class BerkeleyTagHomeTest < MiniTest::Test
     setup do
       fire_fox_with_secure_proxy
       @proxy.new_har
-      visit "#{BW_BASE_URL}/immune-system"
+      @url  = "#{BW_BASE_URL}/immune-system" + "?foo=#{rand(36**8).to_s(36)}"
       @page = Berkeley::BerkeleyHomePage.new(:driver =>@driver, :proxy => @proxy)
+      visit @url
     end
 
     ##################################################################
@@ -17,6 +18,33 @@ class BerkeleyTagHomeTest < MiniTest::Test
         assets = @page.assets
         assets.validate
         assert_equal(true, assets.errors.empty?, "#{assets.errors.messages}")
+      end
+    end
+
+    #########################################################################
+    ################### ADS, ANALYTICS, OMNITURE ############################
+    context "ads, analytics, omniture" do
+      should "not have any errors" do 
+        ad_site           = 'cm.pub.berkwell'
+        ad_categories     = []
+        exclusion_cat     = ""
+        sponsor_kw        = ''
+        thcn_content_type = "tags"
+        thcn_super_cat    = ""
+        thcn_category     = ""
+        ads               = Berkeley::BerkeleyHomePage::AdsTestCases.new(:driver => @driver,
+                                                                 :proxy => @proxy, 
+                                                                 :url => @url,
+                                                                 :ad_site => ad_site,
+                                                                 :ad_categories => ad_categories,
+                                                                 :exclusion_cat => exclusion_cat,
+                                                                 :sponsor_kw  => sponsor_kw,
+                                                                 :thcn_content_type => thcn_content_type,
+                                                                 :thcn_super_cat => thcn_super_cat,
+                                                                 :thcn_category => thcn_category,
+                                                                 :ugc => "[\"n\"]") 
+        ads.validate
+        assert_equal(true, ads.errors.empty?, "#{ads.errors.messages}")
       end
     end
   end
