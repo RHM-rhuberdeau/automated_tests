1b5fa53216a4e0df743cb35795bfa98ccf9856e6
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-09-18 12:12:05 -0400

    Adding cache busting and SEO tests

--- test/integration/berkeley_wellness/explainer/hard_look_bpa.rb
+++ test/integration/berkeley_wellness/explainer/hard_look_bpa.rb
@@ -6,8 +6,19 @@ class BerkeleyExplainerTest < MiniTest::Test
     setup do
       fire_fox_with_secure_proxy
       @proxy.new_har
-      visit "#{BW_BASE_URL}/healthy-community/environmental-health/article/hard-look-bpa"
+      @url  = "#{BW_BASE_URL}/healthy-community/environmental-health/article/hard-look-bpa" + "?foo=#{rand(36**8).to_s(36)}"
       @page = Berkeley::BerkeleyHomePage.new(:driver =>@driver, :proxy => @proxy)
+      visit @url
+    end
+
+    ##################################################################
+    ################### SEO ##########################################
+    context "SEO" do 
+      should "have valid seo" do 
+        seo = @page.seo
+        seo.validate
+        assert_equal(true, seo.errors.empty?, "#{seo.errors.messages}")
+      end
     end
 
     ##################################################################
