ec1d02845b366d5183dbe78ff1c9b1196f4c0f77
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-09-18 12:03:07 -0400

    Adding SEO tests and ability to support query string in Omniture tests

--- test/integration/healthcentral/quiz/do_you_know_how_to_manage_pain_test.rb
+++ test/integration/healthcentral/quiz/do_you_know_how_to_manage_pain_test.rb
@@ -34,11 +34,13 @@ class QuizTest < MiniTest::Test
       end
     end
 
-    ###################################################################
-    #################### SEO ##########################################
-    context "SEO" do 
+    ##################################################################
+    ################### SEO ##########################################
+    context "SEO safe" do 
       should "have the correct title" do 
-        assert_equal(true, (@driver.title == "Do You Know How to Manage Your Pain? - Rheumatoid Arthritis"), "Page title was: #{@driver.title}")
+        seo = @page.seo(:driver => @driver) 
+        seo.validate
+        assert_equal(true, seo.errors.empty?, "#{seo.errors.messages}")
       end
     end
 
@@ -62,7 +64,7 @@ class QuizTest < MiniTest::Test
                                                             :ugc => "[\"n\"]") 
         ads.validate
 
-        omniture = @page.omniture
+        omniture = @page.omniture(:url => @url)
         omniture.validate
         assert_equal(true, (ads.errors.empty? && omniture.errors.empty?), "#{ads.errors.messages} #{omniture.errors.messages}")
       end
--- test/integration/healthcentral/quiz/do_you_know_your_skin_cancer_risk_test.rb
+++ test/integration/healthcentral/quiz/do_you_know_your_skin_cancer_risk_test.rb
@@ -36,9 +36,11 @@ class QuizTest < MiniTest::Test
 
     ##################################################################
     ################### SEO ##########################################
-    context "SEO" do 
+    context "SEO safe" do 
       should "have the correct title" do 
-        assert_equal(true, (@driver.title == "Do You Know Your Skin Cancer Risk? - Skin Cancer"), "Page title was: #{@page.driver.title}")
+        seo = @page.seo(:driver => @driver) 
+        seo.validate
+        assert_equal(true, seo.errors.empty?, "#{seo.errors.messages}")
       end
     end
 
@@ -62,7 +64,7 @@ class QuizTest < MiniTest::Test
                                                             :ugc => "[\"n\"]")
         ads.validate
 
-        omniture = @page.omniture
+        omniture = @page.omniture(:url => @url)
         omniture.validate
         assert_equal(true, (ads.errors.empty? && omniture.errors.empty?), "#{ads.errors.messages} #{omniture.errors.messages}")
       end
