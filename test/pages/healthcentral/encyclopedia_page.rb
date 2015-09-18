4a69cd288e7d093121e6d5ee05f5bb9ff11eca01
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-09-18 12:20:57 -0400

    Removing duplicated code

--- test/pages/healthcentral/encyclopedia_page.rb
+++ test/pages/healthcentral/encyclopedia_page.rb
@@ -14,10 +14,6 @@ module HealthCentralEncyclopedia
     def functionality
       Functionality.new(:driver => @driver, :proxy => @proxy)
     end
-
-    def global_test_cases
-      GlobalTestCases.new(:driver => @driver, :head_navigation => @head_navigation, :footer => @footer)
-    end
   end
 
   class Functionality
@@ -26,32 +22,6 @@ module HealthCentralEncyclopedia
     def initialize(args)
       @driver = args[:driver]
       @proxy  = args[:proxy]
-    end
-  end
-
-  class GlobalTestCases
-    include ::ActiveModel::Validations
-
-    validate :head_navigation
-    validate :footer
-
-    def initialize(args)
-      @head_navigation = args[:head_navigation]
-      @footer          = args[:footer]
-    end
-
-    def head_navigation
-      @head_navigation.validate
-      unless @head_navigation.errors.empty?
-        self.errors.add(:head_navigation, @head_navigation.errors.values.first)
-      end
-    end
-
-    def footer
-      @footer.validate
-      unless @footer.errors.empty?
-        self.errors.add(:footer, @footer.errors.values.first)
-      end
     end
   end
 end
