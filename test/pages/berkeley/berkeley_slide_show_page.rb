d84c1962e93f646b7a4446fd3d21257fd2e4629d
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-09-18 12:36:59 -0400

    Updating page object

--- test/pages/berkeley/berkeley_slide_show_page.rb
+++ test/pages/berkeley/berkeley_slide_show_page.rb
@@ -1,44 +1,73 @@
 require_relative './berkeley_page'
 
-class BerkeleySlideShowPage < BerkeleyPage
-  attr_reader :driver, :proxy
+module BerkeleySlideshow
+  class SlideshowPage < BerkeleyPage
+    attr_reader :driver, :proxy
 
-  def initialize(args)
-  	@driver = args[:driver]
-  	@proxy	= args[:proxy]
-  	@old_ad_calls = []
+    def initialize(args)
+    	@driver = args[:driver]
+    	@proxy	= args[:proxy]
+    	@old_ad_calls = []
+    end
+
+    def seo
+      SEO.new(:driver => @driver)
+    end
+
+    def old_ad_calls
+    	@old_ad_calls
+    end
+
+    def old_ad_calls=(value)
+    	@old_ad_calls = value
+    end
+
+    def slideshow
+    	driver.find_elements(:css, "ul.slides").first
+    end
+
+    def slides
+    	driver.find_elements(:css, "div.flex-viewport ul.slides li")
+    end
+
+    def ads_on_page
+    	all_ads = all_ad_calls
+    	current_ad_calls = all_ads - old_ad_calls
+    	old_ad_calls = all_ads
+    	current_ad_calls
+    end
+
+    def all_ad_calls
+    	all_ad_calls = []
+    	proxy.har.entries.find do |entry|
+    	  if entry.request.url.include?('ad.doubleclick.net/N3965')
+    	  	all_ad_calls << entry.request.url
+    	  end
+    	end
+    	all_ad_calls
+    end
   end
 
-  def old_ad_calls
-  	@old_ad_calls
-  end
+  class SEO
+    include ::ActiveModel::Validations
 
-  def old_ad_calls=(value)
-  	@old_ad_calls = value
-  end
+    validate :one_canonical_link
 
-  def slideshow
-  	driver.find_elements(:css, "ul.slides").first
-  end
+    def initialize(args)
+      @driver = args[:driver]
+    end
 
-  def slides
-  	driver.find_elements(:css, "div.flex-viewport ul.slides li")
-  end
+    def one_canonical_link
+      #Yes this is actually necessary
+      #Apparently <link> and <a> are found separately
 
-  def ads_on_page
-  	all_ads = all_ad_calls
-  	current_ad_calls = all_ads - old_ad_calls
-  	old_ad_calls = all_ads
-  	current_ad_calls
-  end
-
-  def all_ad_calls
-  	all_ad_calls = []
-  	proxy.har.entries.find do |entry|
-  	  if entry.request.url.include?('ad.doubleclick.net/N3965')
-  	  	all_ad_calls << entry.request.url
-  	  end
-  	end
-  	all_ad_calls
+      link_tags   = @driver.find_elements(:css, "link")
+      anchor_tags = @driver.find_elements(:css, "a")
+      all_links   = link_tags + anchor_tags
+      all_links   = all_links.select { |x| x.attribute('rel') == "canonical"}.compact
+      unless all_links.length == 1
+        self.errors.add(:seo, "Expected one canonical link not #{all_links.length}")
+      end
+    end
   end
 end
