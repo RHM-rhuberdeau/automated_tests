a885be89d724b9f86d8fe20a4d6342b0cce06ac0
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-10-14 16:34:12 -0400

    fixing typo

--- test/integration/healthcentral/phases/ibd_introduction.rb
+++ test/integration/healthcentral/phases/ibd_introduction.rb
@@ -11,7 +11,7 @@ class IbdIntroductionDesktop < MiniTest::Test
       phase_fixture     = OpenStruct.new(fixture[0]['ibd_introduction'])
       head_navigation   = HealthCentralHeader::RedesignHeader.new(:logo => "#{ASSET_HOST}/sites/all/themes/healthcentral/images/logo_lbln.png", 
                                    :sub_category => "Digestive Health",
-                                   :related => ['Acid Refulx'],
+                                   :related => ['Acid Reflux'],
                                    :driver => @driver)
       footer            = HealthCentralFooter::RedesignFooter.new(:driver => @driver)
       @page             = Phases::PhasePage.new(:driver => @driver,:proxy => @proxy,:fixture => phase_fixture, :head_navigation => head_navigation, :footer => footer, :collection => false)
