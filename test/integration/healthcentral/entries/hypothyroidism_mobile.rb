8f6b0b6f0af041189be73307dfd89f487a244861
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-10-14 11:35:44 -0400

    Adding a new page

---
+++ test/integration/healthcentral/entries/hypothyroidism_mobile.rb
@@ -0,0 +1,91 @@
+require_relative '../../../minitest_helper' 
+require_relative '../../../pages/healthcentral/redesign_entry_page'
+
+class Hypothyroidism < MiniTest::Test
+  context "a mobile custom program" do 
+    setup do 
+      mobile_fire_fox_with_secure_proxy
+      @proxy.new_har
+      io = File.open('test/fixtures/healthcentral/entries.yml')
+      entry_fixture     = YAML::load_documents(io)
+      @entry_fixture    = OpenStruct.new(entry_fixture[0]['hypothyroidism_mobile'])
+      head_navigation   = HealthCentralHeader::CustomProgramHeader.new(:driver => @driver, :subject => "Living with Hypothyroidism")
+      footer            = HealthCentralFooter::RedesignFooter.new(:driver => @driver)
+      @page             = ::RedesignEntry::RedesignEntryPage.new(:driver => @driver,:proxy => @proxy,:fixture => @entry_fixture, :head_navigation => head_navigation, :footer => footer)
+      @url              = "#{HC_BASE_URL}/more-conditions/c/174035/177245/hypothyroidism/" + "?foo=#{rand(36**8).to_s(36)}"
+      visit @url
+    end
+
+    ##################################################################
+    ################ FUNCTIONALITY ###################################
+    context "when functioning properly" do 
+      should "not have any errors" do 
+        functionality = @page.functionality(:author_name => "Yumhee Park", :author_role => "Editor", :nofollow_author_links => false, :profile_link => "#{HC_BASE_URL}/profiles/c/174035")
+        functionality.validate
+        assert_equal(true, functionality.errors.empty?, "#{functionality.errors.messages}")
+      end
+    end
+
+    ##################################################################
+    ################### ASSETS #######################################
+    context "assets" do 
+      should "have valid assets" do 
+        assets = @page.assets(:base_url => @url)
+        assets.validate
+        assert_equal(true, assets.errors.empty?, "#{assets.errors.messages}")
+      end
+    end
+
+    ##################################################################
+    ################### SEO ##########################################
+    context "SEO safe" do 
+      should "have the correct title" do 
+        seo = @page.seo(:driver => @driver) 
+        seo.validate
+        assert_equal(true, seo.errors.empty?, "#{seo.errors.messages}")
+      end
+    end
+
+    #########################################################################
+    ################### ADS, ANALYTICS, OMNITURE ############################
+    context "ads, analytics, omniture" do
+      should "not have any errors" do 
+        pharma_safe   = true
+        has_file      = @page.analytics_file
+        ad_site       = "cm.own.tcc"
+        ad_categories = ["synthroid","",""]
+        ads           = HealthCentralAds::AdsTestCases.new(:driver => @driver,
+                                                           :proxy => @proxy, 
+                                                           :url => @url,
+                                                           :ad_site => ad_site,
+                                                           :ad_categories => ad_categories,
+                                                           :exclusion_cat => "",
+                                                           :sponsor_kw => '',
+                                                           :thcn_content_type => "SharePosts",
+                                                           :thcn_super_cat => "Healthy Living",
+                                                           :thcn_category => "Diet and Fitness",
+                                                           :ugc => "[\"n\"]") 
+        ads.validate
+
+        omniture = @page.omniture(:url => @url)
+        omniture.validate
+        assert_equal(true, (ads.errors.empty? && omniture.errors.empty?), "#{ads.errors.messages} #{omniture.errors.messages}")
+      end
+    end
+
+    ##################################################################
+    ################### GLOBAL SITE TESTS ############################
+    context "Global Site tests" do 
+      should "have passing global test cases" do 
+        global_test_cases = @page.global_test_cases
+        global_test_cases.validate
+        assert_equal(true, global_test_cases.errors.empty?, "#{global_test_cases.errors.messages}")
+      end
+    end
+  end
+
+  def teardown  
+    @driver.quit  
+    @proxy.close
+  end 
+end
