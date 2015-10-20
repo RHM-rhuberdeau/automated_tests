17e16276b58d33b5f9638fb3726b52b3a84ebfc2
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-10-20 14:16:50 -0400

    Adding a footer object

--- test/pages/the_body/concerns/footer.rb
+++ test/pages/the_body/concerns/footer.rb
@@ -1,9 +1,35 @@
 module TheBodyFooter
   class RedesignFooter
     include ::ActiveModel::Validations
+
+    validate :footer_links
     
     def initialize(args)
       @driver = args[:driver]
+    end
+
+    def footer_links
+      links = @driver.find_elements(:css, ".HC-external-links a")
+      unless links.length == 10
+        self.errors.add(:footer, "Missing links from the footer")
+      end
+    end
+  end
+
+  class RedesignMobileFooter
+    include ::ActiveModel::Validations
+
+    validate :footer_links
+    
+    def initialize(args)
+      @driver = args[:driver]
+    end
+
+    def footer_links
+      links = @driver.find_elements(:css, ".HC-external-links a")
+      unless links.length == 10
+        self.errors.add(:footer, "Missing links from the footer")
+      end
     end
   end
 end
