b4c5b0532c6f2a4d83065123d2854fd72902a8e3
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-10-20 14:17:15 -0400

    Adding a header object for the new article template

--- test/pages/the_body/concerns/header.rb
+++ test/pages/the_body/concerns/header.rb
@@ -28,4 +28,175 @@ module TheBodyHeader
       end
     end
   end
+
+  class RedesignHeader
+    include ::ActiveModel::Validations
+
+    validate :targeted_populations
+    validate :primary_site_sections
+    validate :social_buttons
+    validate :patient_journey_nav
+
+    def initialize(args)
+      @driver = args[:driver]
+    end
+
+    def targeted_populations
+      expected_links  = ['Gay Men', 'Women', 'African Americans', 'Latinos', 'Aging', 'More...']
+      links           = @driver.find_elements(:css, "#navbar-content > div > div.header-table.header-top-row > div:nth-child(2) a")
+      links           = links.select {|x| x.displayed? }
+      if links 
+        link_texts = links.collect { |x| x.text } 
+      else
+        link_texts = []
+      end
+      expected_links.each do |link|
+        unless link_texts.include?(link)
+          self.errors.add(:header, "Targeted populations was missing the #{link} link")
+        end
+      end
+    end
+
+    def primary_site_sections
+      expected_links  = ['JUST DIAGNOSED', 'HIV PREVENTION', 'HIV TREATMENT', 'LIVING WELL', 'COMMUNITY', 'ASK THE EXPERTS']
+      links           = @driver.find_elements(:css, ".header-table.header-bottom-row a")
+      links           = links.select {|x| x.displayed? }
+      if links 
+        link_texts = links.collect { |x| x.text } 
+      else
+        link_texts = []
+      end
+      expected_links.each do |link|
+        unless link_texts.include?(link)
+          self.errors.add(:header, "Primary site section was missing the #{link} link")
+        end
+      end
+    end
+
+    def social_buttons
+      fb      = find "#navbar-content .HC-header-socialButtons .js-Social--Follow-actionable-facebook"
+      twitter = find "#navbar-content .HC-header-socialButtons .js-Social--Follow-actionable-twitter" 
+      news    = find "#navbar-content .HC-header-socialButtons .js-Social--Follow-actionable-newsletters"
+
+      unless fb
+        self.errors.add(:header, "Header was missing the FB social share icon")
+      end
+      unless twitter
+        self.errors.add(:header, "Header was missing the Twitter social share icon")
+      end
+      unless news
+        self.errors.add(:header, "Header was missing the Newsletter social share icon")
+      end
+    end
+
+    def patient_journey_nav
+      buttons = @driver.find_elements(:css, "a.Button--AZ.js-HC-nav-action")
+      buttons.first.click if buttons
+      wait_for { @driver.find_element(:css, "ul.Nav-listGroup-list--Featured").displayed? }
+
+      health_tools_links  = @driver.find_elements(:css, "ul.Nav-listGroup-list--HealthTools a")
+      general_links       = @driver.find_elements(:css, "ul.Nav-listGroup-list--General a")
+      featured_links      = @driver.find_elements(:css, "ul.Nav-listGroup-list--Featured a")
+
+      unless health_tools_links.length == 8
+        self.errors.add(:header, "Health tools section was missing links")
+      end
+      unless general_links.length == 36
+        self.errors.add(:header, "General links section was missing links")
+      end
+      unless featured_links.length == 8
+        self.errors.add(:header, "Featured links section was missing links")
+      end
+    end
+  end
+
+  class RedesignMobileHeader
+    include ::ActiveModel::Validations
+
+    validate :targeted_populations
+    validate :primary_site_sections
+    validate :social_buttons
+    # validate :patient_journey_nav
+
+    def initialize(args)
+      @driver = args[:driver]
+    end
+
+    def targeted_populations
+      hamburger = find "#header-hamburger"
+      hamburger.click if hamburger
+      wait_for { @driver.find_element(:css, "#header-menu .section.resource-centers a").displayed? }
+
+      expected_links  = ['Gay Men', 'Women', 'African Americans', 'Latinos', 'Aging', 'More...']
+      links           = @driver.find_elements(:css, "#header-menu .section.resource-centers a")
+      links           = links.select {|x| x.displayed? }
+      if links 
+        link_texts = links.collect { |x| x.text } 
+      else
+        link_texts = []
+      end
+      expected_links.each do |link|
+        unless link_texts.include?(link)
+          self.errors.add(:header, "Targeted populations was missing the #{link} link")
+        end
+      end
+    end
+
+    def primary_site_sections
+      expected_links  = ['JUST DIAGNOSED', 'HIV PREVENTION', 'HIV TREATMENT', 'LIVING WELL', 'COMMUNITY', 'ASK THE EXPERTS']
+      links           = @driver.find_elements(:css, "#header-menu .section.portals a")
+      links           = links ? links.select  { |x| x.displayed? } : nil
+      link_texts      = links ? links.collect { |x| x.text }       : []
+
+      expected_links.each do |link|
+        unless link_texts.include?(link)
+          self.errors.add(:header, "Primary site section was missing the #{link} link")
+        end
+      end
+    end
+
+    def social_buttons
+      fb      = find "div.bottom-social-bar .social-icon span.icon-facebook"
+      twitter = find "div.bottom-social-bar .social-icon span.icon-twitter" 
+      news    = find "div.bottom-social-bar .social-icon-mail"
+
+      unless fb
+        self.errors.add(:header, "Header was missing the FB social share icon")
+      end
+      unless twitter
+        self.errors.add(:header, "Header was missing the Twitter social share icon")
+      end
+      unless news
+        self.errors.add(:header, "Header was missing the Newsletter social share icon")
+      end
+    end
+
+    def patient_journey_nav
+      buttons = @driver.find_elements(:css, "a.Button--AZ.js-HC-nav-action")
+      begin
+        buttons.first.click if buttons
+      rescue Selenium::WebDriver::Error::ElementNotVisibleError
+      end
+
+      wait_for { @driver.find_element(:css, "ul.Nav-listGroup-list--Featured").displayed? }
+
+      health_tools_links  = @driver.find_elements(:css, "ul.Nav-listGroup-list--HealthTools a")
+      general_links       = @driver.find_elements(:css, "ul.Nav-listGroup-list--General a")
+      featured_links      = @driver.find_elements(:css, "ul.Nav-listGroup-list--Featured a")
+
+      health_tools_links  = health_tools_links ? health_tools_links.select {|x| x.displayed? } : []
+      general_links       = general_links ? general_links.select {|x| x.displayed? } : []
+      featured_links      = featured_links ? featured_links.select {|x| x.displayed? } : []
+
+      unless health_tools_links.length == 8
+        self.errors.add(:header, "Health tools section was missing links")
+      end
+      unless general_links.length == 36
+        self.errors.add(:header, "General links section was missing links")
+      end
+      unless featured_links.length == 8
+        self.errors.add(:header, "Featured links section was missing links")
+      end
+    end
+  end
 end
