d0180b7d0f37ff1ba39e7e7f3d83f44db3bfb8cb
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-10-27 09:26:38 -0400

    Making sure each test has a unique name

--- test/integration/healthcentral/entries/turning_point_a_woman_faces_her_ms_test_mobile.rb
+++ test/integration/healthcentral/entries/turning_point_a_woman_faces_her_ms_test_mobile.rb
@@ -1,7 +1,7 @@
 require_relative '../../../minitest_helper' 
 require_relative '../../../pages/healthcentral/redesign_entry_mobile_page'
 
-class TurningPointEntryPageTest < MiniTest::Test
+class TurningPointMobileEntryPageTest < MiniTest::Test
   context "an expert entry" do 
     setup do 
       mobile_fire_fox_with_secure_proxy
