0b8789810a7f3dcd57d4301074b7c060310879fd
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-11-12 15:02:02 -0500

    Updating subcategory tests to use the new subcategory page object. Adding header and footer tests and removing a test that always yielded a false negative

--- test/integration/healthcentral/subcategory/vision_care_test.rb
+++ test/integration/healthcentral/subcategory/vision_care_test.rb
@@ -9,90 +9,23 @@ class VisionCareSubCategory < MiniTest::Test
       io = File.open('test/fixtures/healthcentral/subcategories.yml')
       subcat_fixture = YAML::load_documents(io)
       @subcat_fixture = OpenStruct.new(subcat_fixture[0]['visioncare'])
-      @page = ::HealthCentral::SubcategoryPage.new(:driver =>@driver,:proxy => @proxy,:fixture => @subcat_fixture)
+      footer          = HealthCentralFooter::RedesignFooter.new(:driver => @driver)
+      header          = HealthCentralHeader::RedesignHeader.new(:logo => "#{ASSET_HOST}/sites/all/themes/healthcentral/images/logo_lbln.png", 
+                                   :sub_category => "Vision Care",
+                                   :related => ["Diabetes" ,"Migraine"],
+                                   :driver => @driver)
+      @page = ::HealthCentral::SubcategoryPage.new(:driver =>@driver,:proxy => @proxy,:fixture => @subcat_fixture, :head_navigation => header, :footer => footer)
       @url  = "#{HC_DRUPAL_URL}/vision-care" + "?foo=#{rand(36**8).to_s(36)}"
       visit @url
     end
 
+    ##################################################################
+    ################ FUNCTIONALITY ###################################
     context "when functioning properly" do 
-      should "have a title in each latest post" do 
-        latest_post_titles = @driver.find_elements(:css, "span.Teaser-title")
-        assert_equal(true, (latest_post_titles.length > 0), "No Latest posts")
-        latest_post_titles = latest_post_titles.collect(&:text)
-        assert_equal(true, (latest_post_titles.length > 0), "No Latest posts")
-        latest_post_titles = latest_post_titles.map {|p| p.gsub(" ", "") }.map {|p| p.gsub("...", "")}
-        latest_post_titles.each do |title|
-          assert_equal(true, (title.length > 0), "Blank title for latest post: #{latest_post_titles}")
-        end
-      end
-
-      should "have a hero post" do 
-        hero_image = @driver.find_element(:css, "div.HeroBox a img")
-        hero_link  = @driver.find_elements(:css, "div.HeroBox a").last
-        hero_link_text = hero_link.text
-        assert_equal(true, !hero_image.nil?, "No hero post image")
-        assert_equal(true, !hero_link.nil?, "No hero post link")
-        assert_equal(true, hero_link_text.length > 0, "No hero post text: #{hero_link_text}")
-      end
-
-      should "have a we recommend section with 3 posts" do 
-        we_recommend_text = @driver.find_element(:css, "h4").text
-        posts = @driver.find_elements(:css, "ul.CollectionListBoxes-list")
-        post_images = @driver.find_elements(:css, "ul.CollectionListBoxes-list li a img")
-        post_links  = @driver.find_elements(:css, "ul.CollectionListBoxes-list li a")
-        post_titles = post_links.collect(&:text)
-
-        assert_equal(3, post_images.length)
-        assert_equal(3, post_links.length)
-        assert_equal(3, post_titles.length)
-        post_titles.each do |title|
-          assert_equal(true, title.length > 0, "Missing title in we recommend post")
-        end
-      end
-
-      should "show up to 15 more latest posts" do 
-        2.times do 
-          wait_for { @driver.find_element(:css, ".js-CollectionEditorsPicks-view-more").displayed? }
-          button = @driver.find_element(:css, ".js-CollectionEditorsPicks-view-more")
-          button.click
-          wait_for { !@driver.find_element(:css, ".spinner-container").displayed? }
-          sleep 0.5
-        end
-        editor_picks = @driver.find_elements(:css, ".Editor-picks-item")
-        assert_equal(true, editor_picks.length >= 5, "#{editor_picks.length} appeared, not 15")
-      end
-
-      should "have a more resources section" do 
-        text  = @driver.find_element(:css, ".Moreresources h4.Block-title").text
-        links = @driver.find_elements(:css, ".Moreresources-container ul li a")
-        links_text = links.collect(&:text)
-
-        assert_equal(true, text.downcase == "more resources", "text was #{text} not More Resources")
-        assert_equal(true, links.length >= 3, "#{links.length} appeared in more resources, not 7")
-        links_text.each do |text|
-          assert_equal(true, (text == "Slideshows" || text == "Medication" || text == "Videos" || text == "Questions" || text == "Topics A-Z" || text == "Quizzes and Assessments" || text == "Blogposts"), "#{text} did not appear in more resources")
-        end
-      end 
-
-      should "have relatlive links in the header" do 
-        links = (@driver.find_elements(:css, ".js-HC-header a") + @driver.find_elements(:css, ".HC-nav-content a") + @driver.find_elements(:css, ".Page-sub-category a")).collect{|x| x.attribute('href')}.compact
-        bad_links = links.map do |link|
-          if (link.include?("healthcentral") && link.index(ASSET_HOST) != 0)
-            link unless link.include?("twitter")
-          end
-        end
-        assert_equal(true, (bad_links.compact.length == 0), "There were links in the header that did not use relative paths: #{bad_links.compact}")
-      end
-
-      should "have relative links in the right rail" do 
-        wait_for { @driver.find_element(:css, ".MostPopular-container").displayed? }
-        links = ((@driver.find_elements(:css, ".Node-content-secondary a") + @driver.find_elements(:css, ".MostPopular-container a")).collect{|x| x.attribute('href')}.compact) - @driver.find_elements(:css, "span.RightrailbuttonpromoItem-title a").collect{|x| x.attribute('href')}.compact
-        bad_links = links.map do |link|
-          if (link.include?("healthcentral") && link.index(ASSET_HOST) != 0)
-            link 
-          end
-        end
-        assert_equal(true, (bad_links.compact.length == 0), "There were links in the header that did not use relative paths: #{bad_links.compact}")
+      should "not have any errors" do 
+        functionality = @page.functionality
+        functionality.validate
+        assert_equal(true, functionality.errors.empty?, "#{functionality.errors.messages}")
       end
     end
 
@@ -145,14 +78,10 @@ class VisionCareSubCategory < MiniTest::Test
     ##################################################################
     ################### GLOBAL SITE TESTS ############################
     context "global site requirements" do
-      should "have a promo item in the right rail" do 
-        wait_for { @driver.find_element(:css, ".RightrailbuttonpromoItem a img").displayed? }
-        promo_img = find ".RightrailbuttonpromoItem a img"
-        promos      = @driver.find_elements(:css, ".RightrailbuttonpromoItem a")
-        promo_text  = promos.last ? promos.last.text : ''
-
-        assert_equal(true, !promo_img.nil?, "promo image did not appear on the page")
-        assert_equal(true, promo_text.length > 0, "promo link text did not appear on the page")
+      should "have passing global test cases" do 
+        global_test_cases = @page.global_test_cases
+        global_test_cases.validate
+        assert_equal(true, global_test_cases.errors.empty?, "#{global_test_cases.errors.messages}")
       end
     end
   end
