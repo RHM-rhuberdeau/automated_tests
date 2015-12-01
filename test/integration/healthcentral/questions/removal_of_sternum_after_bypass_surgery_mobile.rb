ae627c05a8b8befbe6affc8d694cb2d4a53707fc
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-12-01 14:54:11 -0500

    Adding mobile tests for this question

---
+++ test/integration/healthcentral/questions/removal_of_sternum_after_bypass_surgery_mobile.rb
@@ -0,0 +1,85 @@
+require_relative '../../../minitest_helper' 
+require_relative '../../../pages/healthcentral/redesign_mobile_question_page'
+
+class HeartDiseaseQuestionPageTest < MiniTest::Test
+  context "a Question with an expert answer" do 
+    setup do 
+      mobile_fire_fox_with_secure_proxy
+      @proxy.new_har
+      io = File.open('test/fixtures/healthcentral/questions.yml')
+      question_fixture = YAML::load_documents(io)
+      @question_fixture = OpenStruct.new(question_fixture[0]['heart_disease_mobile'])
+      head_navigation   = HealthCentralHeader::MobileRedesignHeader.new(:logo => "#{ASSET_HOST}/sites/all/themes/healthcentral/images/logo_lbln.png", 
+                                   :sub_category => "Heart Disease",
+                                   :related_links => ['High Blood Pressure', 'Cholesterol', 'Diabetes', 'Menopause', 'Obesity'],
+                                   :driver => @driver)
+      footer            = HealthCentralFooter::RedesignFooter.new(:driver => @driver)
+      @page             = RedesignQuestion::RedesignMobileQuestionPage.new(:driver =>@driver,:proxy => @proxy,:fixture => @question_fixture, :head_navigation => head_navigation, :footer => footer)
+      @url              = "#{HC_BASE_URL}/heart-disease/c/question/67255/40783" + $_cache_buster
+      visit @url
+    end
+
+    ##################################################################
+    ################### SEO ##########################################
+    context "SEO safe" do 
+      should "have the correct title" do 
+        seo = @page.seo(:driver => @driver) 
+        seo.validate
+        assert_equal(true, seo.errors.empty?, "#{seo.errors.messages}")
+      end
+    end
+
+    ##################################################################
+    ################### ASSETS #######################################
+    context "assets" do 
+      should "have valid assets" do 
+        assets = @page.assets(:base_url => @url)
+        assets.validate
+        assert_equal(true, assets.errors.empty?, "#{assets.errors.messages}")
+      end
+    end
+
+    #########################################################################
+    ################### ADS, ANALYTICS, OMNITURE ############################
+    context "ads, analytics, omniture" do
+      should "not have any errors" do 
+        pharma_safe   = true
+        has_file      = @page.analytics_file
+        ad_site       = "cm.ver.heartdisease"
+        ad_categories = ["heartdisease","smokingcessation",""]
+        ads           = RedesignQuestion::RedesignMobileQuestionPage::LazyLoadedAds.new(:driver => @driver,
+                                                           :proxy => @proxy, 
+                                                           :url => @url,
+                                                           :ad_site => ad_site,
+                                                           :ad_categories => ad_categories,
+                                                           :exclusion_cat => "",
+                                                           :sponsor_kw => '',
+                                                           :thcn_content_type => "Questions",
+                                                           :thcn_super_cat => "Body & Mind",
+                                                           :thcn_category => "Heart Health",
+                                                           :ugc => "n",
+                                                           :trigger_point => "li.Ad--container") 
+        ads.validate
+
+        omniture = @page.omniture(:url => @url)
+        omniture.validate
+        assert_equal(true, (ads.errors.empty? && omniture.errors.empty?), "#{ads.errors.messages} #{omniture.errors.messages}")
+      end
+    end
+
+
+    #################################################################
+    ################## GLOBAL SITE TESTS ############################
+    context "Global site requirements" do 
+      should "have passing global test cases" do 
+        global_test_cases = @page.global_test_cases
+        global_test_cases.validate
+        assert_equal(true, global_test_cases.errors.empty?, "#{global_test_cases.errors.messages}")
+      end
+    end
+  end
+
+  def teardown  
+    cleanup_driver_and_proxy
+  end 
+end
