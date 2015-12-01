9030eac89cf0642209eb345a093c41513a0e91d1
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2015-12-01 14:37:05 -0500

    Touching up mobile tests for this question

--- test/integration/healthcentral/questions/acking_all_over_hard_to_sleep_mobile.rb
+++ test/integration/healthcentral/questions/acking_all_over_hard_to_sleep_mobile.rb
@@ -1,33 +1,23 @@
 require_relative '../../../minitest_helper' 
-require_relative '../../../pages/healthcentral/redesign_question_page'
+require_relative '../../../pages/healthcentral/redesign_mobile_question_page'
 
-class SkinCareQuestionPageTest < MiniTest::Test
-  context "a question with lots of community answers" do 
+class SkinCareMobileQuestionPageTest < MiniTest::Test
+  context "a moible question with lots of community answers" do 
     setup do 
-      fire_fox_with_secure_proxy
+      mobile_fire_fox_with_secure_proxy
       @proxy.new_har
       io = File.open('test/fixtures/healthcentral/questions.yml')
       question_fixture = YAML::load_documents(io)
-      @question_fixture = OpenStruct.new(question_fixture[0][132858])
-      head_navigation   = HealthCentralHeader::RedesignHeader.new(:logo => "#{ASSET_HOST}/sites/all/themes/healthcentral/images/logo_lbln.png", 
+      @question_fixture = OpenStruct.new(question_fixture[0]['skin_mobile'])
+      head_navigation   = HealthCentralHeader::MobileRedesignHeader.new(:logo => "#{ASSET_HOST}/sites/all/themes/healthcentral/images/logo_lbln.png", 
                                    :sub_category => "Skin Care",
-                                   :related => ['Skin Cancer'],
+                                   :related_links => ['Skin Cancer'],
                                    :driver => @driver)
       footer            = HealthCentralFooter::RedesignFooter.new(:driver => @driver)
-      @page = RedesignQuestion::RedesignQuestionPage.new(:driver => @driver ,:proxy => @proxy,:fixture => @question_fixture, :head_navigation => head_navigation, :footer => footer)
+      @page = RedesignQuestion::RedesignMobileQuestionPage.new(:driver => @driver ,:proxy => @proxy,:fixture => @question_fixture, :head_navigation => head_navigation, :footer => footer)
       @url  = "#{HC_BASE_URL}/skin-care/c/question/550423/132858" + $_cache_buster
       visit @url
     end
-
-    # ##################################################################
-    # ################ FUNCTIONALITY ###################################
-    # context "when functioning properly" do 
-    #   should "not have any errors" do 
-    #     functionality = @page.functionality(:driver => @driver, :phase => "introduction", :phase_navigation => ['Introduction', 'Diagnosis', '', 'Living With', 'Treatment', 'Care', 'Related Conditions'])
-    #     functionality.validate
-    #     assert_equal(true, functionality.errors.empty?, "#{functionality.errors.messages}")
-    #   end
-    # end
 
     ##################################################################
     ################### SEO ##########################################
@@ -57,7 +47,7 @@ class SkinCareQuestionPageTest < MiniTest::Test
         has_file      = @page.analytics_file
         ad_site       = "cm.ver.skin"
         ad_categories = ["skinhealth","acne",""]
-        ads           = HealthCentralAds::AdsTestCases.new(:driver => @driver,
+        ads           = RedesignQuestion::RedesignMobileQuestionPage::LazyLoadedAds.new(:driver => @driver,
                                                            :proxy => @proxy, 
                                                            :url => @url,
                                                            :ad_site => ad_site,
@@ -67,7 +57,8 @@ class SkinCareQuestionPageTest < MiniTest::Test
                                                            :thcn_content_type => "Questions",
                                                            :thcn_super_cat => "Body & Mind",
                                                            :thcn_category => "Skin Health",
-                                                           :ugc => "n") 
+                                                           :ugc => "n",
+                                                           :trigger_point => "li.Ad--container") 
         ads.validate
 
         omniture = @page.omniture(:url => @url)
@@ -83,10 +74,6 @@ class SkinCareQuestionPageTest < MiniTest::Test
         global_test_cases = @page.global_test_cases
         global_test_cases.validate
         assert_equal(true, global_test_cases.errors.empty?, "#{global_test_cases.errors.messages}")
-
-        subnav = @driver.find_element(:css, "div.Page-category.Page-sub-category.js-page-category")
-        title_link = @driver.find_element(:css, ".Page-category-titleLink")
-        sub_category_links = @driver.find_element(:link, "Skin Cancer")
       end
     end
   end
