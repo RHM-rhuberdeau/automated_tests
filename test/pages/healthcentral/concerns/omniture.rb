f0ff9c62239fe4c309aa527e0ba5ab58b511ac42
RHM-rhuberdeau <rhuberdeau@remedyhealthmedia.com>
2016-01-04 14:13:51 -0500

    Trying to fix an error that happening on the remote test machine

--- test/pages/healthcentral/concerns/omniture.rb
+++ test/pages/healthcentral/concerns/omniture.rb
@@ -18,7 +18,7 @@ module HealthCentralOmniture
       @fixture  = args[:fixture]
       @url      = args[:url]
       raise OmnitureIsBlank unless args[:omniture_text]
-      array     = args[:omniture_text].lines
+      array     = args[:omniture_text].lines.to_a
       index     = array.index { |x| x.include?("pageName") }
       raise OmnitureIsBlank unless index
       range     = array.length - index
